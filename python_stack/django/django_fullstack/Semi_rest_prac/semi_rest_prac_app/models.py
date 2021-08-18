dbe3c44554d3396d39bfa0a1788023a6ece9ab04
NickW <nicholas.watson@mailfence.com>
2021-08-17 19:05:20 -0700

    Semi_restful_TV

--- python_stack/django/django_fullstack/Semi_rest_prac/semi_rest_prac_app/models.py
+++ python_stack/django/django_fullstack/Semi_rest_prac/semi_rest_prac_app/models.py
@@ -1,18 +1,30 @@
 from django.db import models
+from datetime import datetime, time
+import re
 
 class ShowManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
-        if len(postData[title]) < 5:
-            errors['title'] = "Movie title name should be at least 5 characters"
-        if len(postData[desc]) < 5:
-            errors['desc'] = "Movie title name should be at least 5 characters"
+
+        # EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        # if not EMAIL_REGEX.match(postData['email']):
+        #     errors['email'] = ("Invalid email address!")
+        print(type(datetime.now()))
+        print(type(postData['release_date']))
+        if len(postData['title']) < 2:
+            errors['title'] = "Movie title should be at least 2 characters"
+        if len(postData['network']) < 2:
+            errors['network'] = "Network title name should be at least 2 characters"
+        if len(postData['release_date']) == 0:
+            errors['release_date'] = "The release date needs to be entered or needs to be in the past"
+        if len(postData['desc']) < 10:
+            errors['desc'] = "Movie title description should be at least 10 characters"
         return errors
 
 class Shows(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
-    release_date = models.DateField(auto_now=True)
+    release_date = models.DateField(null=True)
     desc = models.TextField(default=None)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- python_stack/django/django_fullstack/Semi_rest_prac/semi_rest_prac_app/views.py
+++ python_stack/django/django_fullstack/Semi_rest_prac/semi_rest_prac_app/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from semi_rest_prac_app.models import *
 
 
 def index(request):
-        context = {
-                'all_shows' : Shows.objects.all()
-        }
-        return render(request, "index.html", context)
+    context = {
+        'all_shows' : Shows.objects.all()
+    }
+    return render(request, "index.html", context)
 
 def new_show(request):
         return render(request, "new_show.html")
 
 def create_show(request):
-        if request.method != "POST":
-            return redirect('/')
-        else:
-            new_show = Shows.objects.create(
-                title = request.POST['title'],
-                network = request.POST['network'],
-                release_date = request.POST['release_date'],
-                desc = request.POST['desc']
-                )
-            print(new_show.id)
-            return redirect (f'/shows/{new_show.id}')
+    if request.method != "POST":
+        return redirect('/')
+
+    errors = Shows.objects.basic_validator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request, value)
+            messages.success(request, "Show unsuccessfully created")
+        return redirect('new')
+
+    else:
+        new_show = Shows.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            release_date = request.POST['release_date'],
+            desc = request.POST['desc']
+            )
+        return redirect (f'/shows/{new_show.id}')
 
 def show_profile(request, show_id):
     queried_show = Shows.objects.get(id = show_id)
@@ -32,23 +41,32 @@ def show_profile(request, show_id):
     return render(request, "show_profile.html", context)
 
 def edit_show(request, show_id):
+
     queried_show = Shows.objects.get(id = show_id)
     context = {
         'queried_show' : queried_show
     }
-    return render(request, f'shows/{queried_show.id}/edit', context)
+    return render(request, 'edit_show.html', context)
 
 def update_show(request, show_id):
-        if request.method != "POST":
-                return redirect('/')
-        else:
-                updated = Shows.objects.get(id=show_id)
-                updated.title = request.POST['title'],
-                updated.network = request.POST['network'],
-                updated.release_date = request.POST['release_date'],
-                updated.desc = request.POST['desc']
-                updated.save()
-        return redirect('/show_profile.html')
+    if request.method != "POST":
+        return redirect('/')
+
+    errors = Shows.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key,value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        show = Shows.objects.get(id = show_id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.release_date = request.POST['release_date']
+        show.desc = request.POST['desc']
+        show.save()
+        messages.success(request, "Show successfully updated")
+        return redirect(f'/shows/{show_id}')
 
 def destroy(request, show_id):
         willdelete = Shows.objects.get(id=show_id)
